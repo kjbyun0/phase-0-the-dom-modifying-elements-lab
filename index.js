563bb30e3a06f7d6148f09806059e0d3d4947ea3
Ki Jong Byun <kjbyun0@gmail.com>
2023-05-31 02:22:44 -0500

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.querySelector('main#main');
+document.body.removeChild(main);
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Ki Jong Byun is the champion"
