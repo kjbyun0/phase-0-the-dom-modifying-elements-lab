410f55f5bfe302ed1c92fd97d27114d6a7f504da
Ki Jong Byun <kjbyun0@gmail.com>
2023-07-12 09:59:31 -0500

    Assignment Done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Write your code here!
-const main = document.querySelector('main#main');
-document.body.removeChild(main);
+const mainElems = document.querySelectorAll('main#main');
+for (let i = 0; i < mainElems.length; i++) {
+    mainElems[i].remove();
+}
 
 const newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-newHeader.textContent = "Ki Jong Byun is the champion"
+newHeader.textContent = 'Ken is the champion';
